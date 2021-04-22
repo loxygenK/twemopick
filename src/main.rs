84814cbac5d1985abfff082021a155e059ad57ad
loxygenK <me@loxygen.dev>
2021-04-22 10:37:46 +0900

    ✨ Generate remote URL from Emoji

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod cmdline;
-mod emoji;
+mod twemoji;
 
 use cmdline::get_command_args;
-use emoji::get_emoji_codepoint;
+use twemoji::{get_twemoji_codepoint, generate_remote_url, EmojiOutputType};
 
 fn main() {
 
     let args = get_command_args();
     let emoji = args.value_of("emoji").expect("wtf - required didn't work");
     println!("Selected: {}", &emoji);
-    println!("Code: {}", get_emoji_codepoint(emoji));
+    println!("Code: {}", get_twemoji_codepoint(emoji));
+    println!("Remote: {}", generate_remote_url(emoji, EmojiOutputType::SVG));
 
 }
--- src/twemoji/mod.rs
+++ src/twemoji/mod.rs
@@ -1,4 +1,9 @@
-pub fn get_emoji_codepoint(emoji: &str) -> String {
+pub enum EmojiOutputType {
+    SVG,
+    Laster
+}
+
+pub fn get_twemoji_codepoint(emoji: &str) -> String {
     // Referred:
     // https://github.com/twitter/twemoji-parser/blob/66059e5da13a0407a49bdbe4b1980e00fcc92b62/src/index.js#L58
     let mut points = Vec::with_capacity(6);
@@ -19,3 +24,13 @@ pub fn get_emoji_codepoint(emoji: &str) -> String {
     return points.join("");
 }
 
+pub fn generate_remote_url(emoji: &str, output: EmojiOutputType) -> String {
+    format!(
+        "https://twemoji.maxcdn.com/v/latest/{}/{}.svg",
+        match output {
+            EmojiOutputType::SVG => "svg",
+            EmojiOutputType::Laster => "72x72"
+        },
+        get_twemoji_codepoint(emoji)
+    ).to_string()
+}
