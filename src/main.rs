2034210f976dfaa3d02b214c41fbbc7e38da12ca
loxygenk <me@loxygen.dev>
2021-06-17 21:51:55 +0900

    ➕ Generate the data-url for the image

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,11 @@ fn main() {
     let args = cmdline::get_command_args();
     let emoji = args.value_of("emoji").expect("wtf - required didn't work");
     let codepoint = emoji::codepoint::get_emoji_codepoint(emoji);
-    let url = emoji::url::get_image_url(&codepoint, emoji::url::ImageType::Svg);
+    let url = emoji::url::get_emoji_dataurl(&codepoint, &emoji::url::ImageType::Raster);
 
     println!("Selected: {}", &emoji);
     println!("Code: {}", &codepoint);
     println!("Url : {}", &url);
 
-    let image_bytes = reqwest::blocking::get(&url)
-        .expect("Receiving emoji image data")
-        .error_for_status()
-        .expect("Checking acceptance of the request")
-        .bytes()
-        .expect("Converting the response into byte");
-
-    println!("Length: {} bytes", image_bytes.len());
-    println!("Encoded:{:?}", image_bytes);
 
 }
