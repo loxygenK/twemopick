95edbeabe58f555d055da72c2ffe9f7d2d54a827
loxygenk <me@loxygen.dev>
2021-06-17 22:39:19 +0900

    🎨 Shorten code

--- src/emoji/codepoint.rs
+++ src/emoji/codepoint.rs
@@ -4,18 +4,19 @@ pub fn get_emoji_codepoint(emoji: &str) -> String {
     let mut points = Vec::with_capacity(6);
     let mut pending: u32 = 0;
 
-    for char in emoji.encode_utf16().map(|e: u16| u32::from(e)) {
+    for char in emoji.encode_utf16().map(u32::from) {
         if pending != 0 {
             points.push(format!(
                 "{:02x}",
                 0x10000 + ((pending - 0xd800) << 10) + (char - 0xdc00)
             ));
         } else if char > 0xd800 && char <= 0xdbff {
-            pending = From::from(char);
+            pending = char;
         } else {
             points.push(format!("{:02x}", char));
         }
     }
-    return points.join("");
+
+    points.join("")
 }
 
