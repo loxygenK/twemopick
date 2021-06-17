4f9dba99e38ffc3647ac8ef8e48e0fcd7469f4b1
loxygenk <me@loxygen.dev>
2021-06-17 22:31:34 +0900

    ✨ Add selection of type and validation

--- src/cmdline/mod.rs
+++ src/cmdline/mod.rs
@@ -7,6 +7,26 @@ pub fn get_command_args<'a>() -> ArgMatches<'a> {
                 .help("Emoji to convert into Twemoji.")
                 .required(true)
                 .takes_value(true)
+                .validator(
+                    |x| if x.chars().count() == 1 {
+                        Ok(())
+                    } else {
+                        Err("only one character is allowed".to_string())
+                    }
+                )
+        )
+        .arg(
+            Arg::with_name("use_svg")
+                .help("Get the SVG data URL.")
+                .short("s")
+                .long("svg")
+                .conflicts_with("use_png")
+        )
+        .arg(
+            Arg::with_name("use_png")
+                .help("Get the PNG data URL.")
+                .short("p")
+                .long("png")
         )
         .get_matches()
 }
